12ecc6ff158959427fe764ddd28a915b136e4af6
VaNomad <sjur.hassel@gmail.com>
2023-01-12 19:14:54 +0000

    update also from lag after rename

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 require("dotenv/config");
 
-function test() {
+function test(expectedSecret) {
   const secretMatches = process.env.MEANING_OF_LIFE == expectedSecret;
   console.log(`The meaning of life is ${process.env.MEANING_OF_LIFE}`);
   console.log(`The secret does${secretMatches ? "" : " not"} match!`);
